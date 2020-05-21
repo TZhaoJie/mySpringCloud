eae57de49bfc20be6b8e59a96da69000965d885a
tanzhaojie <1174578722@qq.com>
2020-05-21 11:13:16 +0800

    tzn

--- service-feign1/src/main/java/com/example/servicefeign1/feignInterface/SchedualServiceHi.java
+++ service-feign1/src/main/java/com/example/servicefeign1/feignInterface/SchedualServiceHi.java
@@ -1,12 +1,20 @@
 package com.example.servicefeign1.feignInterface;
 
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(value = "service-client1")
+@FeignClient(value = "service-client1",fallback = SchedualServiceHiHystric.class)
 public interface SchedualServiceHi {
     @RequestMapping(value = "/hi",method = RequestMethod.GET)
     String sayHiFromClientOne(@RequestParam(value = "name") String name);
+}
+@Component
+class SchedualServiceHiHystric implements SchedualServiceHi {
+    @Override
+    public String sayHiFromClientOne(String name) {
+        return "sorry "+name;
+    }
 }
--- service-feign2/src/main/java/com/example/servicefeign2/feignInterface/SchedualServiceHi.java
+++ service-feign2/src/main/java/com/example/servicefeign2/feignInterface/SchedualServiceHi.java
@@ -1,13 +1,20 @@
 package com.example.servicefeign2.feignInterface;
 
-//import com.example.servicefeign2.hystric.SchedualServiceHiHystric;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(value = "service-client2")
+@FeignClient(value = "service-client2",fallback = SchedualServiceHiHystric.class)
 public interface SchedualServiceHi {
     @RequestMapping(value = "/hi",method = RequestMethod.GET)
     String sayHiFromClientOne(@RequestParam(value = "name") String name);
+}
+@Component
+class SchedualServiceHiHystric implements SchedualServiceHi {
+    @Override
+    public String sayHiFromClientOne(String name) {
+        return "sorry "+name;
+    }
 }
